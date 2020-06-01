486153eaba5c2b8b882290764749f9d2fe12a3d0
Megan Gomes <megan.gomes25@gmail.com>
2020-06-01 06:44:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 
   def match(word_array)
     word_array.select do |word|
-    some.split("").sort == @word.split("").sort
+    wordlit("").sort == @word.split("").sort
     end
   end
 end
